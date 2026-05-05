731121df5545a4f174c2bf2b4240902fc84e992f
NULL <badlisjaden@gmail.com>
2026-05-05 18:20:07 +0800

    ActivityDone

--- Assets/Scripts/AgentArrivalCheck.cs
+++ Assets/Scripts/AgentArrivalCheck.cs
@@ -4,32 +4,44 @@ using UnityEngine.AI;
 public class AgentArrivalCheck : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public Transform destinationPosA, destinationPosB, destinationPosC;
+    public Transform[] patrolPoints;
+    private int pointIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(destinationPosA.position);
+        //agent.SetDestination(destinationPos.position);
+        NextPoint();
     }
-
+    void NextPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+        agent.destination = patrolPoints[pointIndex].position;
+        pointIndex = (pointIndex + 1) % patrolPoints.Length;
+    }
     // Update is called once per frame
     void Update()
     {
+        if(!agent.pathPending && agent.remainingDistance < 0.1f)
+            NextPoint();
+      
         //agent.SetDestination(destinationPos.position);
-        if (HasReachedDestination(agent))
-        {
-            Debug.Log("I've Reached my Destination!");
-        }
+        //if (HasReachedDestination(agent))
+        //{
+        //    Debug.Log("I've Reached my Destination!");
+        //}
     }
 
-    bool HasReachedDestination(NavMeshAgent _agent)
-    {
-        if(agent.remainingDistance < agent.stoppingDistance) //Distance
-            return false;
-        if(agent.pathPending) //If AI have another path
-            return false;
-        if (agent.hasPath && agent.velocity.sqrMagnitude > 0) //If AI has path
-            return false;
-        return true;
-    }
+    
+
+    //bool HasReachedDestination(NavMeshAgent _agent)
+    //{
+    //    if(agent.remainingDistance < agent.stoppingDistance) //Distance
+    //        return false;
+    //    if(agent.pathPending) //If AI have another path
+    //        return false;
+    //    if (agent.hasPath && agent.velocity.sqrMagnitude > 0) //If AI has path
+    //        return false;
+    //    return true;
+    //}
 }
